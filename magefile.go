e801f67da876de46f91ca0f003ed43b14c359393
Filippo Costa <filippocosta.italy@gmail.com>
2021-09-29 04:00:00 +0200

    Fix ldpath handling

--- magefile.go
+++ magefile.go
@@ -11,39 +11,34 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 
 	"github.com/magefile/mage/mg"
 )
 
-func setLdLibraryPath(artifactsDir string) {
-	ldVar := os.Getenv("LD_LIBRARY_PATH")
-	ldPaths := strings.Split(ldVar, ":")
-
-	if ldVar != "" {
-		ldVar += ":"
-	}
-	if ldVar == "" || !strings.Contains(ldPaths[len(ldPaths)-1], artifactsDir) {
-		ldVar = ldVar + filepath.Join(artifactsDir, "bins-Linux-release")
-		ldVar = ldVar + ":" + filepath.Join(artifactsDir, "bins-macOS-release")
-		ldVar = ldVar + ":" + filepath.Join(artifactsDir, "bins-Windows-release")
-		os.Setenv("LD_LIBRARY_PATH", ldVar)
-	}
+func libPath(artifactsDir string) string {
+	path := filepath.Join(artifactsDir, "bins-Linux-release")
+	path += ":" + filepath.Join(artifactsDir, "bins-macOS-release")
+	path += ":" + filepath.Join(artifactsDir, "bins-Windows-release")
+	return path
 }
 
 func DownloadArtifactsToDir(dir string) error {
-	if _, err := os.Stat(filepath.Join(dir, "bins-Linux-release.zip")); err == nil {
+	linuxZip := filepath.Join(dir, "bins-Linux-release.zip")
+	gitkeep := filepath.Join(dir, ".gitkeep")
+
+	if _, err := os.Stat(linuxZip); err == nil {
 		fmt.Printf("Artifact files are already present.\n")
 		return nil
 	}
 
 	os.RemoveAll(dir)
 	os.MkdirAll(dir, os.ModePerm)
-	os.OpenFile(filepath.Join(dir, ".gitkeep"), os.O_RDONLY|os.O_CREATE, 0666)
+	os.OpenFile(gitkeep, os.O_RDONLY|os.O_CREATE, os.ModePerm)
 
-	script := "cmd/svm-download-artifacts/svm-download-artifacts.go"
 	token := os.Getenv("GITHUB_TOKEN")
 	fmt.Printf("Using the GitHub token '%s'", token)
+
+	script := "cmd/svm-download-artifacts/svm-download-artifacts.go"
 	cmd := exec.Command("go", "run", script, "--token", token, "--dest", dir)
 	cmd.Env = os.Environ()
 	cmd.Stdout = os.Stdout
@@ -56,12 +51,6 @@ func DownloadArtifactsToDir(dir string) error {
 }
 
 func Build() error {
-	here, _ := os.Getwd()
-	dir := filepath.Join(here, "svm", "artifacts")
-	mg.Deps(mg.F(DownloadArtifactsToDir, dir))
-
-	setLdLibraryPath(dir)
-
 	cmd := exec.Command("go", "mod", "download")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -69,10 +58,16 @@ func Build() error {
 }
 
 func Install() error {
+	here, _ := os.Getwd()
+	dir := filepath.Join(here, "svm", "artifacts")
+
 	mg.Deps(Build)
+	mg.Deps(mg.F(DownloadArtifactsToDir, dir))
 
 	cmd := exec.Command("go", "install", "./...")
+	cmd.Env = append(cmd.Env, fmt.Sprintf("LD_LIBRARY_PATH=%s", libPath(dir)))
 	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	return cmd.Run()
 }
 
@@ -80,11 +75,13 @@ func Test() error {
 	mg.Deps(Build)
 	mg.Deps(Install)
 
-	fmt.Printf("LD_LIBRARY_PATH IS %s\n\n", os.Getenv("LD_LIBRARY_PATH"))
+	here, _ := os.Getwd()
+	dir := filepath.Join(here, "svm", "artifacts")
 
 	cmd := exec.Command("go", "test", "-p", "1", ".")
-	here, _ := os.Getwd()
 	cmd.Dir = filepath.Join(here, "svm")
+	cmd.Env = append(cmd.Env, fmt.Sprintf("LD_LIBRARY_PATH=%s", libPath(dir)))
+	cmd.Env = append(cmd.Env, fmt.Sprintf("DYLD_LIBRARY_PATH=%s", libPath(dir)))
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
