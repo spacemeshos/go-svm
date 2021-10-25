f7effa2a43e9ce5a8ff0afd8920bb1312ebd271d
Filippo Costa <filippocosta.italy@gmail.com>
2021-10-25 14:05:21 +0200

    Fix spacing

--- svm/api.go
+++ svm/api.go
@@ -2,8 +2,8 @@ package svm
 
 // #cgo CFLAGS: -I${SRCDIR}/artifacts/
 // #cgo LDFLAGS: -L${SRCDIR}/artifacts/
-// #cgo linux LDFLAGS: -lsvm -lm -ldl -Wl -rpath
-// #cgo darwin LDFLAGS: -lsvm -lm -ldl -framework Security -framework Foundation -Wl, -rpath,@loader_path
+// #cgo linux LDFLAGS: -lsvm -lm -ldl -Wl,-rpath
+// #cgo darwin LDFLAGS: -lsvm -lm -ldl -framework Security -framework Foundation -Wl,-rpath,@loader_path
 // #cgo windows LDFLAGS: -lsvm -lm
 //
 // #include "svm.h"
